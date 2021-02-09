1d6e18babdfb29405bf5efb54fd819f0e510dab6
xiaohui li <xiaohuili@xiaohuideMacBook-Pro.local>
2021-02-09 14:16:08 -0500

    Initial commit

---
+++ src/tests/actions/expenses.test.js
@@ -0,0 +1,22 @@
+
+import { TestScheduler } from 'jest';
+import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+
+test('should setup remove expense action object', () => {
+    const action = removeExpense({ id: '123abc'});
+    expect(action).toEqual({
+        type: 'REMOVE_EXPENSE',
+        id: '123abc'
+    });
+});
+
+test('should setup edit expense action object', () => {
+    const action = editExpense( '123abc', { note: 'New note value' });
+    expect(action).toEqual({
+        type: 'EDIT_EXPENSE',
+        id: '123abc',
+        updates: {
+            note: 'New note value'
+        }
+    });
+});
